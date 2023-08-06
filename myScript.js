f221a4c6a636711c8e4d925406aedb41d9070c42
Tryndameir <andrewdinho99@gmail.com>
2023-08-06 18:11:31 -0400

    Added a class for transitioning to when button is mouse hovered

--- myScript.js
+++ myScript.js
@@ -6,15 +6,20 @@ const buttons = document.querySelectorAll(".button");
 
     buttons.forEach(button => {
         button.addEventListener('mouseover', () => {
-           button.style.cssText = 'border: solid; border-width: 3px; border-color: yellow;';
+           button.classList.add('hovered');
+           //button.style.cssText = 'border: solid; border-width: 3px; border-color: yellow;';
            
         });
     })
 
+    //function myFunction (e) {
+       // if (e.propertyName !== 'mouseover') return;
+       // this.classList.remove('hovered');
+   // }
+
     buttons.forEach(button => {
-        button.addEventListener('mouseleave', () => {
-           button.style.cssText = 'border: solid; border-width: 3px; border-color: black;';
-           
+        button.addEventListener('mouseout', () => {
+            button.classList.remove('hovered');   
         });
     })
         
